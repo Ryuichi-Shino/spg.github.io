4954534bce3498e41c5174df6ca5e0b76ce1418c
Shintaro MIKAMI <s1f102001917@iniad.org>
2022-09-15 23:29:05 +0900

    aiueo

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from game import views as game_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('game/', game_views.index),
 ]
