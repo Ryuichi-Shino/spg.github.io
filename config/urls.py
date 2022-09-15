9d0aab0f7d87d1e00902c7570a0ebf6952ec9f84
Ryuichi-Shino <s1f102001018@iniad.org>
2022-09-15 23:51:50 +0900

    a

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from game import views as game_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game/', game_views.index),
+    path('game/', include('game.urls')),
 ]
