08bf194c6ed7ddec98f3aa0cd078bb995c68b5b8
Ryuichi-Shino <s1f102001018@iniad.org>
2022-09-22 23:46:24 +0900

    スマホで見る

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ["192.168.0.15", "192.168.0.8"] # ローカルIPアドレスを追加
