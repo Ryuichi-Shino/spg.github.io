b552ee0861346bc68067aab95c864551f1f9df14
Ryuichi-Shino <s1f102001018@iniad.org>
2022-09-24 18:45:19 +0900

    文字サイズ変更ボタン
    大小のindexページ作ってむりやり

--- game/views.py
+++ game/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'game/index.html')
 
+def move_to_smallindex(request):
+    return render(request, 'game/smallindex.html')
+
+def move_to_bigindex(request):
+    return render(request, 'game/bigindex.html')
+
 def move_to_gamepage(request):
     return render(request, 'game/gamepage.html')
 
@@ -13,6 +19,6 @@ def move_to_gamepage2(request):
 def move_to_gamepage3(request):
     return render(request, 'game/gamepage3.html')
 
-def move_to_oputionpage(request):
-    return render(request, 'game/opution.html')
+def move_to_optionpage(request):
+    return render(request, 'game/option.html')
 
