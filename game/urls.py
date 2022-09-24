91353ae1174fb3d2d01a3926ef887ac4699d6598
Shintaro MIKAMI <s1f102001917@iniad.org>
2022-09-24 20:49:37 +0900

    aaaa

--- game/urls.py
+++ game/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('templates/', views.move_to_gamepage, name='move_to_gamepage'),
     path('templates2/', views.move_to_gamepage2, name='move_to_gamepage2'),
     path('templates3/', views.move_to_gamepage3, name='move_to_gamepage3'),
-    path('opution/',views.move_to_oputionpage, name='move_to_oputionpage'),
-
+    path('smallindex/',views.move_to_smallindex, name='move_to_smallindex'),
+    path('bigindex/',views.move_to_bigindex, name='move_to_bigindex'),
+    path('option/',views.move_to_optionpage, name='move_to_optionpage'),
 ]
