c6193a960ea365845c9de4813d1ede11d7a3451c
藤本稜人 <s1F102003942@iniad.org>
2022-10-03 13:31:55 +0900

    a

--- game/static/js/style.js
+++ game/static/js/style.js
@@ -8,3 +8,9 @@ function count(){
 function result(){
     return count
 }
+
+//sound//
+function audio() {
+  document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
+  document.getElementById('btn_audio').play(); //クリックしたら音を再生
+}
