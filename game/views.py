66a08989f8303233886f5d90073632253ad6131e
藤本稜人 <s1F102003942@iniad.org>
2022-10-04 15:34:17 +0900

    a

--- game/views.py
+++ game/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import sys
 sys.dont_write_bytecode = True
 from game.models import Score
+from django.contrib import messages
 
 def index(request):
 	return render(request, 'game/index.html')
@@ -25,17 +26,6 @@ def move_to_gamepage3(request):
 def move_to_optionpage(request):
     return render(request, 'game/option.html')
 
-def move_to_quiz3(request):
-    return render(request, 'game/quiz3.html')
-
-def move_to_quiz4(request):
-    return render(request, 'game/quiz4.html')
-
-def move_to_quiz5(request):
-    return render(request, 'game/quiz5.html')
-
-def move_to_result(request):
-    return render(request, 'game/quiz_result.html')
 
 def move_to_quiz1(request):
     try:
@@ -56,7 +46,64 @@ def move_to_quiz2(request):
 
     if request.method=='POST':
         ans = request.POST['answer']
-        if (ans == 'a'):
+        if (ans == 'correct'):
             score.increment()
     context = { 'score':score }
     return render(request, "game/quiz2.html", context)
+    
+def move_to_quiz3(request):
+    try:
+        score = Score.objects.get(pk=1)
+    except Score.DoesNotExist:
+        score = Score(score=0)
+        score.save()
+
+    if request.method=='POST':
+        ans = request.POST['answer']
+        if (ans == 'correct'):
+            score.increment()
+    context = { 'score':score }
+    return render(request, "game/quiz3.html", context)
+
+def move_to_quiz4(request):
+    try:
+        score = Score.objects.get(pk=1)
+    except Score.DoesNotExist:
+        score = Score(score=0)
+        score.save()
+
+    if request.method=='POST':
+        ans = request.POST['answer']
+        if (ans == 'correct'):
+            score.increment()
+    context = { 'score':score }
+    return render(request, "game/quiz4.html", context)
+
+def move_to_quiz5(request):
+    try:
+        score = Score.objects.get(pk=1)
+    except Score.DoesNotExist:
+        score = Score(score=0)
+        score.save()
+
+    if request.method=='POST':
+        ans = request.POST['answer']
+        if (ans == 'correct'):
+            score.increment()
+    context = { 'score':score }
+    return render(request, "game/quiz5.html", context)
+
+
+def move_to_result(request):
+    try:
+        score = Score.objects.get(pk=1)
+    except Score.DoesNotExist:
+        score = Score(score=0)
+        score.save()
+
+    if request.method=='POST':
+        ans = request.POST['answer']
+        if (ans == 'correct'):
+            score.increment()
+    context = { 'score':score }
+    return render(request, "game/quiz_result.html", context)
